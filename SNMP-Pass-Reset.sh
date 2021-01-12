3d8ea8fe1dc6fa517c887ca3c57b94307840a819
Adam Forester <aforester@silverdogstudios.com>
2021-01-12 10:31:29 -0600

    v2

--- SNMP-Pass-Reset.sh
+++ SNMP-Pass-Reset.sh
@@ -9,7 +9,11 @@ done
 printf "\nBuilding SNMP Reset Commands per CMA. Be Patient...\n"
 for I in $(cat CMA-IP.txt)
 do
-  mgmt_cli -d $I -r true show gateways-and-servers details-level full limit 500 --format json |jq -r --arg q "'" --arg w '"' --arg i $I '.objects[] | select(.type == "simple-gateway") | "mgmt_cli -d " + $i + "  -r true run-script script-name SNMP script " + $q + "clish -c " + $w + "set snmp usm user sts security-level authPriv auth-pass-phrase vpn123456 privacy-pass-phrase vpn123456 privacy-protocol AES authentication-protocol SHA1" +$w + $q + " targets " + .name' >>$CMA_NAME-update-snmp.txt
+  mgmt_cli -d $I -r true show gateways-and-servers details-level full limit 500 --format json |jq -r --arg q "'" --arg w '"' --arg i $I '.objects[] | select(.type == "simple-gateway") | "mgmt_cli -d " + $i + "  -s id.txt run-script script-name SNMP script " + $q + "clish -c " + $w + "set snmp usm user sts security-level authPriv auth-pass-phrase vpn123456 privacy-pass-phrase vpn123456 privacy-protocol AES authentication-protocol SHA1" +$w + $q + " targets " + .name' >>$CMA_NAME-update-snmp.txt
+  sed -i "1s/^/mgmt_cli -d $DOMAIN -r true login > id.txt\n/" $CMA_NAME-update-snmp.txt
+  sed -i '1s/^/echo Deleting objects do not stop script\n/' $CMA_NAME-update-snmp.txt
+  echo "mgmt_cli -s id.txt publish" >> $CMA_NAME-update-snmp.txt
+  echo "mgmt_cli -s id.txt logout" >> $CMA_NAME-update-snmp.txt
 done
 
 rm CMA-IP.txt
